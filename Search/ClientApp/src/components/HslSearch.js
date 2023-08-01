b7dfeea8406690f7ee5369d2fe8abb4f8150c40b
Veikko Lintujärvi <veikko@lintujarvi.fi>
2023-08-01 19:44:56 +0300

    Style improvement, spinner added

--- Search/ClientApp/src/components/HslSearch.js
+++ Search/ClientApp/src/components/HslSearch.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { BiBus, BiTrain } from 'react-icons/bi';
+import { BiBus, BiTrain, BiSearchAlt } from 'react-icons/bi';
+import SyncLoader from 'react-spinners/SyncLoader';
 
 export const HslSearch = () => {
   const [routes, setRoutes] = useState([]);
@@ -30,24 +31,42 @@ export const HslSearch = () => {
     <div>
       <h1>HSL aikatauluhaku</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={{ padding: '1rem' }}>
         <fieldset>
           <label>
-            <h2>Hae linjan numerolla</h2>
-            <input name='term' required onChange={handleChange} />
+            <h4>Hae linjan numerolla</h4>
+            <input
+              name='term'
+              required
+              onChange={handleChange}
+              style={{ marginTop: '0.5rem', marginBottom: '1rem' }}
+            />
+            <button
+              type='submit'
+              style={{
+                paddingLeft: 10,
+                backgroundColor: 'transparent',
+                border: 'none',
+              }}
+            >
+              <BiSearchAlt fontSize={25} />
+            </button>
           </label>
         </fieldset>
-        <button type='submit'>Hae</button>
       </form>
       {loading ? (
-        <p>Lataa...</p>
+        <SyncLoader loading={loading} style={{ marginLeft: '3rem' }} />
       ) : (
         <ul>
           {routes.map((route) => {
             return (
               <li key={route.shortName}>
-                ({route.shortName}) {route.longName}{' '}
-                {route.mode === 'BUS' ? <BiBus /> : <BiTrain />}
+                {route.mode === 'BUS' ? (
+                  <BiBus style={{ color: 'green' }} />
+                ) : (
+                  <BiTrain style={{ color: 'blue' }} />
+                )}{' '}
+                ({route.shortName}) {route.longName}
               </li>
             );
           })}
